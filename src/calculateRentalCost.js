021532ed145ad9ee997d38b21012b6414b1b9b4c
Agnieszka <agbiel.22@gmail.com>
2026-04-08 13:34:20 +0200

    next code

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,19 +4,22 @@
  * @return {number}
  */
 
+
 const COST_PER_DAY = 40;
 const DISCOUNT_3_DAYS = 20;
 const DISCOUNT_7_DAYS = 50;
 
+const MIN_DAYS_FOR_SMALL_DISCOUNT = 3;
+const MIN_DAYS_FOR_LARGE_DISCOUNT = 7;
+
 function calculateRentalCost(days) {
+  const baseCost = days * COST_PER_DAY;
 
- const baseCost = days * COST_PER_DAY;
-
-  if (days >= 7) {
+  if (days >= MIN_DAYS_FOR_LARGE_DISCOUNT) {
     return baseCost - DISCOUNT_7_DAYS;
   }
 
-  if (days >= 3) {
+  if (days >= MIN_DAYS_FOR_SMALL_DISCOUNT) {
     return baseCost - DISCOUNT_3_DAYS;
   }
 
